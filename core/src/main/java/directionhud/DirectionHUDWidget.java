f8c7239eadce358de2fe2c6bfbffaccab857665e
Adler7 <rauer.torben1@gmail.com>
2022-12-13 22:46:06 +0100

    push

--- core/src/main/java/directionhud/DirectionHUDWidget.java
+++ core/src/main/java/directionhud/DirectionHUDWidget.java
@@ -5,10 +5,8 @@ import net.labymod.api.client.gui.hud.hudwidget.SimpleHudWidget;
 import net.labymod.api.client.gui.hud.position.HudSize;
 import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.mouse.MutableMouse;
-import net.labymod.api.client.render.batch.ResourceRenderContext;
 import net.labymod.api.client.render.matrix.Stack;
 import net.labymod.api.client.resources.ResourceLocation;
-import net.labymod.api.util.bounds.Rectangle;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unchecked")
@@ -26,22 +24,37 @@ public class DirectionHUDWidget extends SimpleHudWidget<DirectionHUDWidgetConfig
   ResourceLocation hudPng = ResourceLocation.create("directionhud",
       "textures/hud.png");
 
-  Icon nadle = Icon.texture(ResourceLocation.create("directionhud", "textures/nadle.png"));
+  final int widthHeight = 38;
 
   @Override
   public void render(@Nullable Stack stack, MutableMouse mouse, float partialTicks,
       boolean isEditorContext, HudSize size) {
-    int widthHeight = 38;
     size.set(widthHeight, widthHeight);
     if (stack == null) {
       return;
     }
+    float yaw = labyAPI.minecraft().clientPlayer().getRotationHeadYaw();
+    stack.push();
+    if (yaw >= 360) {
+      yaw -= 360;
+    }
+    stack.translate(size.getWidth() / 2, size.getHeight() / 2, 0);
+    stack.rotate(yaw, 0, 0, 1);
     labyAPI.renderPipeline().resourceRenderer()
         .size(widthHeight, widthHeight)
         .pos(0, 0)
         .texture(hudPng)
         .sprite(0, 0, 256)
         .render(stack);
-    nadle.render(stack, 0, 0, widthHeight);
+    stack.pop();
+    renderNeedle(stack, widthHeight);
+  }
+
+  final Icon needleIcon = Icon.texture(
+      ResourceLocation.create("directionhud", "textures/needle.png"));
+
+  private Stack renderNeedle(Stack stack, int size) {
+    needleIcon.render(stack, 0, 0, size);
+    return stack;
   }
 }
