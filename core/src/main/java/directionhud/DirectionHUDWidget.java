6dbbe8ad8df052ee9931c219f92083f8912b6646
Adler7 <rauer.torben1@gmail.com>
2022-12-13 23:07:39 +0100

    push

--- core/src/main/java/directionhud/DirectionHUDWidget.java
+++ core/src/main/java/directionhud/DirectionHUDWidget.java
@@ -24,6 +24,9 @@ public class DirectionHUDWidget extends SimpleHudWidget<DirectionHUDWidgetConfig
   ResourceLocation hudPng = ResourceLocation.create("directionhud",
       "textures/hud.png");
 
+  final Icon needleIcon = Icon.texture(
+      ResourceLocation.create("directionhud", "textures/needle.png"));
+
   final int widthHeight = 38;
 
   @Override
@@ -38,23 +41,19 @@ public class DirectionHUDWidget extends SimpleHudWidget<DirectionHUDWidgetConfig
     if (yaw >= 360) {
       yaw -= 360;
     }
-    stack.translate(size.getWidth() / 2, size.getHeight() / 2, 0);
-    stack.rotate(yaw, 0, 0, 1);
     labyAPI.renderPipeline().resourceRenderer()
         .size(widthHeight, widthHeight)
         .pos(0, 0)
         .texture(hudPng)
         .sprite(0, 0, 256)
         .render(stack);
+    stack.push();
+    float centerX = this.size.getWidth() / 2F;
+    float centerY = this.size.getHeight() / 2F;
+    stack.translate(centerX, centerY, 0);
+    stack.rotate(yaw, 0, 0, 1);
+    stack.translate(-centerX, -centerY, 0);
+    needleIcon.render(stack, 0, 0, widthHeight);
     stack.pop();
-    renderNeedle(stack, widthHeight);
-  }
-
-  final Icon needleIcon = Icon.texture(
-      ResourceLocation.create("directionhud", "textures/needle.png"));
-
-  private Stack renderNeedle(Stack stack, int size) {
-    needleIcon.render(stack, 0, 0, size);
-    return stack;
   }
 }
